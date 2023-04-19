3f6731feed44e3e36035fb065c593c141e4d7a65
Pavel Vakarash <pavelvakaras@gmail.com>
2023-04-19 23:00:02 +0200

    fix-2

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -12,12 +12,12 @@ import PigPage from '../PigPage/PigPage';
 function App(): JSX.Element {
   return (
     <Routes>
-      <Route path="/animals-best-friends" element={<Layout />}>
-        <Route path="/animals-best-friends/home" element={<HomePage />} />
-        <Route path="/animals-best-friends/wolf" element={<WolfPage />} />
-        <Route path="/animals-best-friends/dog" element={<DogPage />} />
-        <Route path="/animals-best-friends/cat" element={<CatPage />} />
-        <Route path="/animals-best-friends/pig" element={<PigPage />} />
+      <Route path="/best-friends-animals" element={<Layout />}>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/wolf" element={<WolfPage />} />
+        <Route path="/dog" element={<DogPage />} />
+        <Route path="/cat" element={<CatPage />} />
+        <Route path="/pig" element={<PigPage />} />
       </Route>
     </Routes>
   );
